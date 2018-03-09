0c71b36a864e3034228209796c90c7d54bde1a11
evan-l-munson <evan.l.munson@gmail.com>
2018-03-09 11:03:56 -0500

    hopefully added ?SAoTD

--- R/SAoTD.R
+++ R/SAoTD.R
@@ -1,3 +1,11 @@
 #' @useDynLib SAoTD
+#' 
+#' @title Sentiment Analysis of Twitter Data
+#' 
+#' @details This analytic is an in initial forray into sentiment analysis.  This analytic will allow a user to access the Twitter API (once they create their own developer account), ingest tweets of their interest, clean / tidy data, perform topic modeling if interested, compute sentiment scores utilizing the Bing Lexicon, and output visualizations.
+#' 
+#' @author Evan Munson, evan.l.munson at gmail.com
+#' 
+#' @references Find a vignette using: vignette("SAoTD)
 #' @importFrom Rcpp evalCpp
-NULL
+"SAoTD"
