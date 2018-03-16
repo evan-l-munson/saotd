60f6eb7f26988223ef954b47afecf9add25881ac
evan-l-munson <evan.l.munson@gmail.com>
2018-03-16 16:22:29 -0400

    fixed my oops of deleting Bing and stop_words.rda

--- R/Functions.R
+++ R/Functions.R
@@ -1202,7 +1202,6 @@ TimeScale <- function(DataFrameTidyScores, HT_Topic) {
 #' @param HT_Topic If using hashtag data select:  "hashtag".  If using topic data select:  "topic".
 #' 
 #' @import ggplot2
-#' @import maps
 #' @importFrom dplyr quo
 #' 
 #' @return A ggplot plot.
