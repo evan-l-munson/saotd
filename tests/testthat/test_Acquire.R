1dbaeed69b60f8e040b24bd2acc7cbc4789f0055
evan-l-munson <evan.l.munson@gmail.com>
2018-04-10 16:50:51 -0400

    tests

---
+++ tests/testthat/test_Acquire.R
@@ -0,0 +1,4 @@
+test_that("The Acquire function is working as properly", {
+  
+ 
+})
--- tests/testthat/test_Tidy.R
+++ tests/testthat/test_Tidy.R
@@ -1,10 +1,10 @@
 test_that("The Tidy function is working as properly", {
-  
+
   text1 <- "I really love my dog, he is the best friend anyone could ever ask for!"
   text2 <- "RT I want @coolguy24 to meet me for #icecream!!!! https://t.co/v4nta536rd"
   text <- c(text1, text2)
   test_Tidy_df <- as.data.frame(text)
-  
+
   true_Tidy_df <- dplyr::tribble(
     ~text, ~Token,
     text1, as.character("love"),
@@ -14,12 +14,12 @@ test_that("The Tidy function is working as properly", {
     text2, as.character("meet"),
     text2, as.character("icecream")
   )
-  
-  true <- correct_Tidy_df$Token
+
+  true <- true_Tidy_df$Token
   test <- SAoTD::Tidy(DataFrame = test_Tidy_df)
   test <- test$Token
-  
+
   expect_equal(test, true)
   expect_error(object = SAoTD::Tidy(DataFrame = text), "The input for this function is a data frame.")
-  
+
 })
