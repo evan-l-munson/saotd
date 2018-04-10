36e666fa1ee924f4881113ebcf5792cb4d828665
evan-l-munson <evan.l.munson@gmail.com>
2018-04-10 14:47:45 -0400

    bigram and trigram tests

---
+++ tests/testthat/test_Bigram.R
@@ -0,0 +1,18 @@
+test_that("Bigrams are computed properly", {
+  
+  text <- "This is the website for “R for Data Science”. 
+  This book will teach you how to do data science with R: 
+  You’ll learn how to get your data into R, get it into the most useful structure, transform it, visualise it and model it." 
+  test_bigram_df <- as.data.frame(x = text)
+  
+  correct_bigram_df <- dplyr::tribble(
+    ~word1, ~word2, ~n,
+    "data", "science", as.integer(2),
+    "structure", "transform", as.integer(1),
+    "youll", "learn", as.integer(1)
+  )
+  
+  expect_equal(SAoTD::Bigram(DataFrame = test_bigram_df), correct_bigram_df)
+  expect_error(object = SAoTD::Bigram(DataFrame = text), "The input for this function is a data frame.")
+  
+})
