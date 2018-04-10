72b51d7495c5a89b93934157c85c918d4a2a3772
evan-l-munson <evan.l.munson@gmail.com>
2018-04-10 11:32:15 -0400

    unigram test fix

--- tests/testthat/test_Unigram.R
+++ tests/testthat/test_Unigram.R
@@ -3,13 +3,15 @@ test_that("Unigrams are computed properly", {
   text <- "I really love my dog, he is the best friend anyone could ever ask for!"
   test_unigram_df <- as.data.frame(x = text)
   
-  correct_unigram_df <- tribble(
+  correct_unigram_df <- dplyr::tribble(
     ~word, ~n,
     "dog", as.integer(1),
     "friend", as.integer(1),
     "love", as.integer(1)
   )
   
-  expect_identical(SAoTD::Unigram(DataFrame = test_unigram_df), correct_unigram_df)
+  expect_equal(SAoTD::Unigram(DataFrame = test_unigram_df), correct_unigram_df)
+  expect_error(object = SAoTD::Unigram(DataFrame = text), "The input for this function is a data frame.")
+  
 })
 
