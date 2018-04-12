31a6362fbc86ad5e5a3abe70089fcd98354dacbf
evan-l-munson <evan.l.munson@gmail.com>
2018-04-12 12:01:06 -0400

    Corpus.Distribution testing

---
+++ tests/testthat/test_CorpusDistribution.R
@@ -0,0 +1,35 @@
+context("test_CorpusDistribution")
+
+# Test Data
+
+test_HT_df <- dplyr::data_frame(
+  text = c("I really love and hate my dog, he is the best most amazing friend anyone could ever ask for!
+           I really hate my love to hate on my stupid dog, he is the worst friend anyone could ever ask for!",
+           "cats are the best most amazing friends anyone could ask for "),
+  hashtag = c("dog", "cat"),
+  created = lubridate::as_datetime(c('2018-02-09 17:56:30', '2018-02-10 18:46:10')),
+  key = c("coolguy123", "crazycatperson1234"))
+
+test_HT_Tidy <- SAoTD::Tidy(DataFrame = test_HT_df)
+test_HT_Tidy_Scores <- SAoTD::Scores(DataFrameTidy = test_HT_Tidy, HT_Topic = "hashtag")
+
+p <- SAoTD::Corpus.Distribution(DataFrameTidyScores = test_HT_Tidy_Scores)
+
+# Tests
+test_that("The Corpus.Distribution function properly ingests data frame", {
+
+  expect_error(object = SAoTD::Corpus.Distribution(DataFrameTidyScores = text), "The input for this function is a data frame.")
+
+})
+
+test_that("The Corpus.Distribution plot retunrs ggplot object", {
+
+  expect_is(p, "ggplot")
+
+})
+
+# test_that("Expect warning", {
+#   
+#   expect_warning(p <- SAoTD::Corpus.Distribution(DataFrameTidyScores = test_HT_Tidy_Scores))
+# })
+  
--- tests/testthat/test_MinScores.R
+++ tests/testthat/test_MinScores.R
@@ -2,43 +2,47 @@
 
 # Data for hashtag without "HT_Topic_Selection"
 test_HT_df <- dplyr::data_frame(
-  text = "I really love and hate my dog, he is the best most amazing friend anyone could ever ask for!",
-  hashtag = "dog", 
-  created = lubridate::as_datetime('2018-02-09 17:56:30'),
-  key = "coolguy123")
+  text = c("I really love and hate my dog, he is the best most amazing friend anyone could ever ask for!  
+  I really hate my love to hate on my stupid dog, he is the worst friend anyone could ever ask for!",
+           "cats are the best most amazing friends anyone could ask for except when they are being miserable horrible terrible demon spawn"),
+  hashtag = c("dog", "cat"), 
+  created = lubridate::as_datetime(c('2018-02-09 17:56:30', '2018-02-10 18:46:10')),
+  key = c("coolguy123", "crazycatperson1234"))
 
 test_HT_Tidy_df <- SAoTD::Tidy(DataFrame = test_HT_df)
 test_HT_Scores_Tidy_df <- SAoTD::Scores(DataFrameTidy = test_HT_Tidy_df, HT_Topic = "hashtag")
 test_HT <- SAoTD::Min.Scores(DataFrameTidyScores = test_HT_Scores_Tidy_df, HT_Topic = "hashtag")
-test_HT <- test_HT$TweetSentiment
+test_HT <- test_HT$TweetSentimentScore[1]
 
-check_HT <- "positive"
+check_HT <- -3
 
 # Data for hashtag with "HT_Topic_Selection"
 test_HT_selection <- SAoTD::Min.Scores(DataFrameTidyScores = test_HT_Scores_Tidy_df, HT_Topic = "hashtag", HT_Topic_Selection = "dog")
-test_HT_selection <- test_HT_selection$TweetSentiment
+test_HT_selection <- test_HT_selection$TweetSentimentScore[1]
 
-check_HT_selection <- "positive"
+check_HT_selection <- -2
 
 # Data for topic without "HT_Topic_Selection"
 test_Topic_df <- dplyr::data_frame(
-  text = "I really love to hate on my stupid dog, he is the worst friend anyone could ever ask for!",
-  Topic = "dog", 
-  created = lubridate::as_datetime('2018-02-09 17:56:30'),
-  key = "coolguy123")
+  text = c("I really love and hate my dog, he is the best most amazing friend anyone could ever ask for!  
+  I really hate my love to hate on my stupid dog, he is the worst friend anyone could ever ask for!",
+           "cats are the best most amazing friends anyone could ask for except when they are being miserable horrible terrible demon spawn"),
+  Topic = c("dog", "cat"), 
+  created = lubridate::as_datetime(c('2018-02-09 17:56:30', '2018-02-10 18:46:10')),
+  key = c("coolguy123", "crazycatperson1234"))
 
 test_Topic_Tidy_df <- SAoTD::Tidy(DataFrame = test_Topic_df)
 test_Topic_Scores_Tidy_df <- SAoTD::Scores(DataFrameTidy = test_Topic_Tidy_df, HT_Topic = "topic")
 test_Topic <- SAoTD::Min.Scores(DataFrameTidyScores = test_Topic_Scores_Tidy_df, HT_Topic = "topic")
-test_Topic <- test_Topic$TweetSentiment
+test_Topic <- test_Topic$TweetSentimentScore[1]
 
-check_Topic <- "negative"
+check_Topic <- -3
 
 # Data for topic without "HT_Topic_Selection"
 test_Topic_selection <- SAoTD::Min.Scores(DataFrameTidyScores = test_Topic_Scores_Tidy_df, HT_Topic = "topic", HT_Topic_Selection = "dog")
-test_Topic_selection <- test_Topic_selection$TweetSentiment
+test_Topic_selection <- test_Topic_selection$TweetSentimentScore[1]
 
-check_Topic_selection <- "negative"
+check_Topic_selection <- -2
 
 # Tests
 test_that("The Min.Scores function properly ingests data frame", {
