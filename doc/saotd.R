11d1a2a5cbadfe1c90c35a01fbf184769419f0fc
evan-l-munson <evan.l.munson@gmail.com>
2019-03-17 13:59:37 -0700

    cached larger graphs in vignette

--- doc/saotd.R
+++ doc/saotd.R
@@ -1,4 +1,7 @@
-## ----warning=FALSE, message=FALSE----------------------------------------
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ----packages, warning=FALSE, message=FALSE------------------------------
 library(saotd)
 library(tidytext)
 library(dplyr)
@@ -6,7 +9,7 @@ library(stringr)
 library(knitr)
 library(utils)
 
-## ----eval=FALSE----------------------------------------------------------
+## ----tweet_acquire, eval=FALSE-------------------------------------------
 #  consumer_key <- "XXXXXXXXXXXXXXXXXXXXXXXXX"
 #  consumer_secret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 #  access_token <- "XXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
@@ -25,50 +28,50 @@ library(utils)
 #  
 #  load("test_tweets.RData")
 
-## ------------------------------------------------------------------------
+## ----raw_tweets----------------------------------------------------------
 data("raw_tweets")
 TD <- raw_tweets
 
-## ------------------------------------------------------------------------
+## ----tidy----------------------------------------------------------------
 TD_Tidy <- saotd::tweet_tidy(DataFrame = TD)
 
 TD_Tidy$Token[3:8] %>% 
   knitr::kable("html")
 
-## ----message=FALSE-------------------------------------------------------
+## ----unigram, message=FALSE----------------------------------------------
 saotd::unigram(DataFrame = TD) %>% 
   dplyr::top_n(10) %>% 
   knitr::kable("html", caption = "Twitter data Uni-Grams")
 
-## ----message=FALSE-------------------------------------------------------
+## ----bigram, message=FALSE-----------------------------------------------
 saotd::bigram(DataFrame = TD) %>% 
   dplyr::top_n(10) %>% 
   knitr::kable("html", caption = "Twitter data Bi-Grams")
 
-## ----message=FALSE-------------------------------------------------------
+## ----trigram, message=FALSE----------------------------------------------
 saotd::trigram(DataFrame = TD) %>% 
   dplyr::top_n(10) %>% 
   knitr::kable("html", caption = "Twitter data Tri-Grams")
 
-## ----message=FALSE, message=FALSE, error=FALSE---------------------------
+## ----merge, message=FALSE, message=FALSE, error=FALSE--------------------
 TD_Merge <- merge_terms(DataFrame = TD, term = "cancelled flight", term_replacement = "cancelled_flight")
 
-## ----message=FALSE-------------------------------------------------------
+## ----merged_unigram, message=FALSE---------------------------------------
 saotd::unigram(DataFrame = TD_Merge) %>% 
   dplyr::top_n(10) %>% 
   knitr::kable("html", caption = "Twitter data Uni-Grams")
 
-## ----message=FALSE-------------------------------------------------------
+## ----merged_bigram, message=FALSE----------------------------------------
 saotd::bigram(DataFrame = TD_Merge) %>% 
   dplyr::top_n(10) %>% 
   knitr::kable("html", caption = "Twitter data Bi-Grams")
 
-## ----message=FALSE-------------------------------------------------------
+## ----merged_trigram, message=FALSE---------------------------------------
 saotd::trigram(DataFrame = TD_Merge) %>% 
   dplyr::top_n(10) %>% 
   knitr::kable("html", caption = "Twitter data Tri-Grams")
 
-## ----fig.align='center'--------------------------------------------------
+## ----bigram_network, fig.align='center'----------------------------------
 TD_Bigram <- saotd::bigram(DataFrame = TD_Merge)
 
 saotd::bigram_network(BiGramDataFrame = TD_Bigram,
@@ -79,7 +82,7 @@ saotd::bigram_network(BiGramDataFrame = TD_Bigram,
                       node_size = 3,
                       set_seed = 1234)
 
-## ----fig.align='center'--------------------------------------------------
+## ----corr_network, fig.align='center'------------------------------------
 TD_Corr <- saotd::word_corr(DataFrameTidy = TD_Tidy, 
                             number = 200, 
                             sort = TRUE)
@@ -91,33 +94,33 @@ saotd::word_corr_network(WordCorr = TD_Corr,
                          node_color = "black", 
                          node_size = 1)
 
-## ------------------------------------------------------------------------
+## ----scores--------------------------------------------------------------
 TD_Scores <- saotd::tweet_scores(DataFrameTidy = TD_Tidy, 
                                  HT_Topic = "hashtag")
 
-## ----fig.align='center'--------------------------------------------------
+## ----posneg_words, fig.align='center'------------------------------------
 saotd::posneg_words(DataFrameTidy = TD_Tidy, 
                     num_words = 10)
 
-## ----fig.align='center'--------------------------------------------------
+## ----filtered_posneg_words, fig.align='center'---------------------------
 saotd::posneg_words(DataFrameTidy = TD_Tidy, 
                     num_words = 10, 
                     filterword = "fail")
 
-## ------------------------------------------------------------------------
+## ----max_scores----------------------------------------------------------
 saotd::tweet_max_scores(DataFrameTidyScores = TD_Scores,
                         HT_Topic = "hashtag")
 
-## ------------------------------------------------------------------------
+## ----min_scores----------------------------------------------------------
 saotd::tweet_min_scores(DataFrameTidyScores = TD_Scores, 
                         HT_Topic = "hashtag")
 
-## ------------------------------------------------------------------------
+## ----filtered_max_scores-------------------------------------------------
 saotd::tweet_max_scores(DataFrameTidyScores = TD_Scores, 
                         HT_Topic = "hashtag", 
                         HT_Topic_Selection = "kitten")
 
-## ----fig.align='center', warning=FALSE, message=FALSE, results="hide"----
+## ----number_topics_plot, fig.align='center', warning=FALSE, message=FALSE, results="hide", cache=TRUE, cache.path='saotd_cache/'----
 saotd::number_topics(DataFrame = TD, 
                      num_cores = 2L, 
                      min_clusters = 2, 
@@ -125,14 +128,14 @@ saotd::number_topics(DataFrame = TD,
                      skip = 1, 
                      set_seed = 1234)
 
-## ----fig.align='center', warning=FALSE, message=FALSE, results='asis'----
+## ----topics, fig.align='center', warning=FALSE, message=FALSE, results='hide', cache=TRUE, cache.path='saotd_cache/'----
 TD_Topics <- saotd::tweet_topics(DataFrame = TD, 
                                  clusters = 5, 
                                  method = "Gibbs", 
                                  set_seed = 1234, 
                                  num_terms = 10)
 
-## ------------------------------------------------------------------------
+## ----rename_topics-------------------------------------------------------
 TD_Topics <- TD_Topics %>% 
   dplyr::mutate(Topic = stringr::str_replace_all(Topic, "^1$", "luggage")) %>% 
   dplyr::mutate(Topic = stringr::str_replace_all(Topic, "^2$", "gate_delay")) %>% 
@@ -140,69 +143,69 @@ TD_Topics <- TD_Topics %>%
   dplyr::mutate(Topic = stringr::str_replace_all(Topic, "^4$", "enjoy")) %>% 
   dplyr::mutate(Topic = stringr::str_replace_all(Topic, "^5$", "other_delay"))
 
-## ------------------------------------------------------------------------
+## ----topic_tidy----------------------------------------------------------
 TD_Topics_Tidy <- saotd::tweet_tidy(DataFrame = TD_Topics)
 TD_Topics_Scores <- saotd::tweet_scores(DataFrameTidy = TD_Topics_Tidy, 
                                         HT_Topic = "topic")
 
-## ------------------------------------------------------------------------
+## ----topic_max_scores----------------------------------------------------
 saotd::tweet_max_scores(DataFrameTidyScores = TD_Topics_Scores,
                         HT_Topic = "topic")
 
-## ------------------------------------------------------------------------
+## ----topic_min_scores----------------------------------------------------
 saotd::tweet_min_scores(DataFrameTidyScores = TD_Topics_Scores,  
                         HT_Topic = "topic")
 
-## ------------------------------------------------------------------------
+## ----topic_filtered_max_scores-------------------------------------------
 saotd::tweet_max_scores(DataFrameTidyScores = TD_Topics_Scores, 
                         HT_Topic = "topic", 
                         HT_Topic_Selection = "United")
 
-## ----fig.align='center'--------------------------------------------------
+## ----corpus_distribution, fig.align='center'-----------------------------
 saotd::tweet_corpus_distribution(DataFrameTidyScores = TD_Scores, 
                                  color = "black", 
                                  fill = "white")
 
-## ----fig.align='center'--------------------------------------------------
+## ----tweet_distribution, fig.align='center'------------------------------
 saotd::tweet_distribution(DataFrameTidyScores = TD_Scores, 
                           binwidth = 1,
                           HT_Topic = "hashtag", 
                           color = "black", 
                           fill = "white")
 
-## ----fig.align='center'--------------------------------------------------
+## ----box_plot, fig.align='center'----------------------------------------
 saotd::tweet_box(DataFrameTidyScores = TD_Scores, 
                  HT_Topic = "hashtag")
 
-## ----fig.align='center'--------------------------------------------------
+## ----violin_plot, fig.align='center'-------------------------------------
 saotd::tweet_violin(DataFrameTidyScores = TD_Scores,
                     HT_Topic = "hashtag")
 
-## ----fig.align='center', warning=FALSE, message=FALSE--------------------
+## ----time_plot, fig.align='center', warning=FALSE, message=FALSE---------
 saotd::tweet_time(DataFrameTidyScores = TD_Scores, 
                   HT_Topic = "hashtag")
 
-## ----fig.align='center'--------------------------------------------------
+## ----topic_corpus_distribution, fig.align='center'-----------------------
 saotd::tweet_corpus_distribution(DataFrameTidyScores = TD_Topics_Scores, 
                                  color = "black", 
                                  fill = "white")
 
-## ----fig.align='center'--------------------------------------------------
+## ----topic_tweet_distribution, fig.align='center'------------------------
 saotd::tweet_distribution(DataFrameTidyScores = TD_Topics_Scores, 
                           binwidth = 1,
                           HT_Topic = "topic", 
                           color = "black", 
                           fill = "white")
 
-## ----fig.align='center'--------------------------------------------------
+## ----topic_box_plot, fig.align='center'----------------------------------
 saotd::tweet_box(DataFrameTidyScores = TD_Topics_Scores, 
                  HT_Topic = "topic")
 
-## ----fig.align='center'--------------------------------------------------
+## ----topic_violin_plot, fig.align='center'-------------------------------
 saotd::tweet_violin(DataFrameTidyScores = TD_Topics_Scores,
                     HT_Topic = "topic")
 
-## ----fig.align='center', warning=FALSE, message=FALSE--------------------
+## ----topic_time, fig.align='center', warning=FALSE, message=FALSE--------
 saotd::tweet_time(DataFrameTidyScores = TD_Topics_Scores, 
                   HT_Topic = "topic")
 
