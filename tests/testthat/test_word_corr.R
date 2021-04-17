a44d7fee472ef36603d66d0b1c4cd688246b660b
Evan Munson <evan.l.munson@gmail.com>
2021-04-16 19:51:24 -0700

    completed word corr testing

--- tests/testthat/test_word_corr.R
+++ tests/testthat/test_word_corr.R
@@ -2,15 +2,53 @@
 testthat::context("Word Correlation")
 
 # Test Data
-test_WordCorr_df <- dplyr::tibble(
-  text = c("I am happy and joyful",
-           "I am sad and annoyed",
-           "I am supremely happy and gratefully annoyed",
-           "I am super duper happy and joyful"),
-  key = c("coolguy123",
-          "whoknowswhat45847",
-          "al;sdkjf8978",
-          "kalsdfj9087"))
+test_WordCorr_df <- tibble::tribble(
+  ~user_id, 
+  ~status_id, 
+  ~created_at, 
+  ~screen_name, 
+  ~text, 
+  ~hashtags, 
+  ~location, 
+  ~key, 
+  ~query,
+as.character(12344), 
+  as.character(098098), 
+  as.POSIXct("2021-04-07 01:15:33"), 
+  as.character("cool123"), 
+  "I am happy and joyful", 
+  as.character("dog"), 
+  as.character("Phoenix AZ"), 
+  as.character("dude123 2021-04-07 01:15:33"), 
+  as.character("#puppies"),
+as.character(987234),
+  as.character(90898), 
+  as.POSIXct("2021-04-07 01:16:43"), 
+  as.character("sweet123"), 
+  "I am sad and annoyed", 
+  as.character("dog"), 
+  as.character("Denver CO"), 
+  as.character("sweet123 2021-04-07 01:16:43"), 
+  as.character("#puppies"),
+as.character(23443), 
+  as.character(5645), 
+  as.POSIXct("2021-04-08 01:17:41"), 
+  as.character("happy123"), 
+  "I am supremely happy and gratefully annoyed",
+  as.character("cat"), 
+  as.character("Ouray CO"), 
+  as.character("happy123 2021-04-08 01:17:41"), 
+  as.character("#cat"),
+as.character(098787), 
+  as.character(8765), 
+  as.POSIXct("2021-04-09 06:17:45"), 
+  as.character("yota123"), 
+  "I am super duper happy and joyful",
+  as.character("kittie"), 
+  as.character("Tucson AZ"), 
+  as.character("yota123 2021-04-09 06:17:45"), 
+  as.character("#kittie")
+)
 
 correct_WordCorr_df <- dplyr::tribble(
   ~item1, ~item2, ~correlation,
@@ -37,7 +75,7 @@ testthat::test_that("Word Correlations has correct input dataframe", {
   testthat::expect_error(
     object = saotd::word_corr(DataFrameTidy = correct_WordCorr_df), 
     "The data frame is not properly constructed.  
-        The data frame must contain at minimum the columns: Token and key.")
+         The data frame must contain at minimum the columns: Token and key.")
   
   testthat::expect_error(
     object = saotd::word_corr(DataFrameTidy = test_WordCorr_Tidy_df, 
