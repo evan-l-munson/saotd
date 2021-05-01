0e7e0ebbcd515df7c7ba48e771302b409080e973
Evan Munson <evan.l.munson@gmail.com>
2021-05-01 12:59:54 -0700

    tweet_max_scores and testing completed

--- function_checks.R
+++ function_checks.R
@@ -91,8 +91,39 @@ score_puppies_topic <- saotd::tweet_scores(
 
 # Max Score ---------------------------------------------------------------
 
-min_score_HT <- 
+max_score_HT <- 
   saotd::tweet_max_scores(
+    DataFrameTidyScores = score_puppies_ht,
+    HT_Topic = "hashtag")
+max_score_HT
+
+# unnest list
+# https://stackoverflow.com/questions/52286482/function-to-extract-all-list-elements-from-a-dataframe-column-into-individual-co
+# https://stackoverflow.com/questions/44832059/r-how-to-extract-a-list-from-a-dataframe
+
+max_score_HT_sel <-
+  saotd::tweet_max_scores(
+    DataFrameTidyScores = score_puppies_ht,
+    HT_Topic = "hashtag",
+    HT_Topic_Selection = "puppy")
+max_score_HT_sel
+
+max_score_topic <- saotd::tweet_max_scores(
+  DataFrameTidyScores = score_puppies_topic, 
+  HT_Topic = "topic")
+max_score_topic
+
+max_score_topic_sel <- saotd::tweet_max_scores(
+  DataFrameTidyScores = score_puppies_topic, 
+  HT_Topic = "topic", 
+  HT_Topic_Selection = 4)
+max_score_topic_sel
+
+
+# Min Scores --------------------------------------------------------------
+
+min_score_HT <- 
+  saotd::tweet_min_scores(
     DataFrameTidyScores = score_puppies_ht,
     HT_Topic = "hashtag")
 min_score_HT
@@ -102,18 +133,18 @@ min_score_HT
 # https://stackoverflow.com/questions/44832059/r-how-to-extract-a-list-from-a-dataframe
 
 min_score_HT_sel <-
-  saotd::tweet_max_scores(
+  saotd::tweet_min_scores(
     DataFrameTidyScores = score_puppies_ht,
     HT_Topic = "hashtag",
     HT_Topic_Selection = "puppy")
 min_score_HT_sel
 
-min_score_topic <- saotd::tweet_max_scores(
+min_score_topic <- saotd::tweet_min_scores(
   DataFrameTidyScores = score_puppies_topic, 
   HT_Topic = "topic")
 min_score_topic
 
-min_score_topic_sel <- saotd::tweet_max_scores(
+min_score_topic_sel <- saotd::tweet_min_scores(
   DataFrameTidyScores = score_puppies_topic, 
   HT_Topic = "topic", 
   HT_Topic_Selection = 4)
